df93a582358a2e4708b4bdad501c10399a13d52c
antonio210 <antonio.blatancic21@gmail.com>
2022-11-29 23:29:09 +0100

    Progress updated...  - starting Section8

---
+++ src/coding/Section7_OOP_Inheritance/exercises/PoolArea/Cuboid.java
@@ -0,0 +1,26 @@
+package coding.Section7_OOP_Inheritance.exercises.PoolArea;
+
+public class Cuboid extends Rectangle{
+    private double height;
+
+
+    public Cuboid(double length, double width, double height){
+        super(length, width);
+        if(height<0){
+            this.height=0;
+        }else{
+            this.height = height;
+        }
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getVolume(){
+        return getArea()*height;
+    }
+
+
+
+}
